13ce88a1ffb7323d908c0736cb7b7f35b55fc325
Im Kyeongbin <fjeidjenk38@gmail.com>
2025-04-01 17:15:45 +0900

    Initial setup for Vue + Flask app in WSL2

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Bookstore App</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('')
+onMounted(async () => {
+  const res = await axios.get('/api/hello')
+  message.value = res.data.message
+})
+</script>
+EOF
